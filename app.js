b364769c9025484d6940d9f78561a8a65cf83621
rtkilian <rhys.kilian12@gmail.com>
2021-08-10 17:14:06 +1000

    Display reviews on campground page

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
+    const campground = await Campground.findById(id).populate('reviews');
     res.render('campgrounds/show', { campground });
 }));
 
