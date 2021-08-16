7de05ea114ea8e6c6e2d9ba639102db72261997b
rtkilian <rhys.kilian12@gmail.com>
2021-08-17 07:26:04 +1000

    Serving static assets

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTES IN SEPARATE FILES
 app.use('/campgrounds', campgrounds);
