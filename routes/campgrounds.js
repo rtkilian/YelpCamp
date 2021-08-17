cba82c7e02dcf54a0100b68e497d54b4fc9aca2c
rtkilian <rhys.kilian12@gmail.com>
2021-08-17 19:42:48 +1000

    Flash error partial added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -34,6 +34,10 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Could not find that campground');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
