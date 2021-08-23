fce4a9d35338314da762586bdbd6fb2114373e8f
rtkilian <rhys.kilian12@gmail.com>
2021-08-24 07:50:25 +1000

    Set up user registration

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -62,16 +63,10 @@ app.use((req, res, next) => {
     next();
 });
 
-// REGISTER USER ROUTES
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({ email: 'coltttt@gmail.com', username: 'coltttt' });
-    const newUser = await User.register(user, 'chicken');
-    res.send(newUser);
-});
-
 // ROUTES IN SEPARATE FILES
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 // INDEX ROUTE
 app.get('/', (req, res) => {
