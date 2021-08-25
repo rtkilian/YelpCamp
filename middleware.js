73d857fe23763bfa0458851a05572cfa6aae17f3
rtkilian <rhys.kilian12@gmail.com>
2021-08-26 07:52:32 +1000

    ReturnTo behaviour

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    req.session.returnTo = req.originalUrl // store the most recent URL in the session
     if (!req.isAuthenticated()) {
         req.flash('error', 'You must be logged in to do this');
         return res.redirect('/login');
--- routes/users.js
+++ routes/users.js
@@ -30,7 +30,9 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds' // redirect to most recent URL that has authentication on it (middleware check)
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res) => {
