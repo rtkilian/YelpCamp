38b6e712a6ddcbf2dbbf625abb5f67cc2197f7b5
rtkilian <rhys.kilian12@gmail.com>
2021-08-26 07:24:18 +1000

    Added logout

--- routes/users.js
+++ routes/users.js
@@ -30,4 +30,10 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect('/campgrounds');
 });
 
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Successfully logged you out');
+    res.redirect('/campgrounds');
+});
+
 module.exports = router;
