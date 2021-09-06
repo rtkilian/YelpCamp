fd75c9e6b21d34204a84dcfd8c85cafa98a452d8
rtkilian <rhys.kilian12@gmail.com>
2021-09-06 18:27:21 +1000

    Reviews delete authorisation

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 const { campgroundSchema, reviewSchema } = require('./schemas');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
+const review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
     req.session.returnTo = req.originalUrl; // store the most recent URL in the session
@@ -25,6 +27,16 @@ module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do this');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do this');
         return res.redirect(`/campgrounds/${id}`);
     }
