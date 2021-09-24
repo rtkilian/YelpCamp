9b21b4fbf86dfe135c842451b8e762332ff3a588
rtkilian <rhys.kilian12@gmail.com>
2021-09-25 08:20:33 +1000

    Added map controls

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -126,3 +126,6 @@ map.on('load', () => {
         map.getCanvas().style.cursor = '';
     });
 });
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
