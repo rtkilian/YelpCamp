c990c098e37853c72ce400776d56437ec6e3ebcc
rtkilian <rhys.kilian12@gmail.com>
2021-10-12 18:54:40 +1100

    Swap between local and Mongo Atlas

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+// const dbUrl = process.env.DB_URL;
 const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
